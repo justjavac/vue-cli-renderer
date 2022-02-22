534ee561fe29828e7cde6350f4766284b9585ab0
迷渡 <justjavac@gmail.com>
2022-02-22 09:50:01 +0800

    简化逻辑
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import Counter from './components/Counter.vue';
-import { Text, View } from './components';
 
 const count = ref(0)
 
-// onMounted(() => {
-//   setTimeout(() => count.value = 1, 1_000);
-// });
+onMounted(() => {
+  setTimeout(() => count.value = 1, 1_000);
+});
 </script>
 
 <template>
- <View>
-    <Text>Hello</Text>
     <Counter :count="count" />
-  </View>
 </template>
