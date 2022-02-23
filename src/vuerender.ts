6a20e9e258e8d1cb4b341ed8f94efc57ce437562
迷渡 <justjavac@gmail.com>
2022-02-23 14:23:50 +0800

    remove comment
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- src/vuerender.ts
+++ src/vuerender.ts
@@ -2,6 +2,7 @@ import { createRenderer } from "@vue/runtime-core";
 import type {
   ComponentPublicInstance,
   CreateAppFunction,
+  RootRenderFunction
 } from "@vue/runtime-core";
 import { nodeOps } from "./nodeOps";
 import { patchProp } from "./patchProp";
@@ -26,5 +27,15 @@ export const createApp: CreateAppFunction<DOMElement> = (
 
   return app;
 };
+
+export const render: RootRenderFunction<DOMElement> = (...args) => {
+  console.log("root render", ...args)
+  const { render } = createRenderer<DOMNode, DOMElement>({
+    patchProp,
+    ...nodeOps,
+  });
+  
+  return render(...args);
+}
 
 export * from "@vue/runtime-core";
